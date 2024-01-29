fdb0d2b744f87a2a767102a66f0e540f2d6c65b8
Lucrada <iamdracula.dracula@gmail.com>
2024-01-29 17:22:13 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,117 +1,94 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+/* eslint-disable prettier/prettier */
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View, Text, StyleSheet, Button, Alert, TouchableOpacity } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const Tab = createBottomTabNavigator();
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const MyTabs = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <Tab.Navigator>
+      <Tab.Screen name="Tab1" component={Tab1} options={{headerShown: false, tabBarLabel: 'Tab1'}} />
+      <Tab.Screen name="Tab2" component={HomeScreen} options={{headerShown: false}} />
+    </Tab.Navigator>
+  );
+};
+
+const Tab1 = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'purple'}}>
+      <Text style={{fontSize: 40, color: '#fff', fontWeight: 'bold'}}>Tab1</Text>
     </View>
   );
-}
+};
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const HomeScreen = ({ navigation }): React.JSX.Element => {
+  return (
+    <View style={styles.homeContainer}>
+      <Text style={styles.mainText}>Home Screen</Text>
+      <Button title="Move to details screen" onPress={() => navigation.navigate('Details', {
+        data: 'This is some data',
+      })} />
+    </View>
+  );
+};
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const DetailScreen = ({route}): React.JSX.Element => {
+  const { data } = route.params;
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.detailsContainer}>
+      <Text style={styles.mainText}>Details Screen</Text>
+      <Text style={{fontSize: 20, color: '#fff'}}>{data}</Text>
+    </View>
   );
-}
+};
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen name="Home" component={HomeScreen} options={{
+          headerShown: false,
+          headerTitleStyle: {
+            color: '#fff',
+          },
+          headerStyle: {
+            backgroundColor: 'purple',
+          },
+          headerRight: () => <Button title="Tap here" onPress={() => Alert.alert("Hello")} />,
+        }} />
+        <Stack.Screen name="Details" component={DetailScreen} options={{
+          headerRight: () => <TouchableOpacity onPress={() => Alert.alert("Info", "This is some info")}><Text style={{color: "crimson", fontSize: 15, fontWeight: "bold"}}>Show Info</Text></TouchableOpacity>
+        }} />
+        <Stack.Screen name="Main" component={MyTabs} options={{headerShown: false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  homeContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'teal',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  detailsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'crimson',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  mainText: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
 
